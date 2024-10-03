ad14743c9e4f645f85adc0394bcffbbc82775b31
진우현 <@naver.com>
2024-10-03 15:14:52 +0900

    충돌3

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+
+
 while True:
     print("\n계산기 프로그램")
     print("1. 더하기")
@@ -14,23 +16,5 @@ while True:
 
     print("충돌 2")
     
-    choice = input("원하는 연산을 선택하세요 (1/2/3/4/5): ")
-    
-    if choice == '3':
-        print("계산기를 종료합니다.")
-        break
-    
-    if choice in ('1', '2'):
-        num1 = float(input("첫 번째 숫자를 입력하세요: "))
-        num2 = float(input("두 번째 숫자를 입력하세요: "))
-        
-        if choice == '1':
-            print(f"{num1} + {num2} = {add(num1, num2)}")
-        elif choice == '2':
-            print(f"{num1} - {num2} = {subtract(num1, num2)}")
-        elif choice == '3':
-            print(f"{num1} * {num2} = {subtract(num1, num2)}")
-        elif choice == '4':
-            print(f"{num1} / {num2} = {subtract(num1, num2)}")
-    else:
-        print("잘못된 입력입니다. 다시 선택해주세요.")
+   
+    print("잘못된 입력입니다. 다시 선택해주세요.")
